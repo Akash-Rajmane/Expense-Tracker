beac80db8735083d42e0655b832996c72f711c7b
Akash <akashrajmane23@gmail.com>
2023-11-10 22:16:30 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ function App() {
   return (
     <div className="App">
      <ExpenseItem/>
-     <div>
-     Food Rs 10
-     </div>
-     <div>
-     Petrol Rs 100
-     </div>
-     <div>
-     Movies Rs 200
-     </div>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021,2,28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  const locationOfExpenditure = "Akluj";
+
   return (
-    <div>Expense Items</div>
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div>{locationOfExpenditure}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
+      </div>
+    </div>
   )
 }
 
