3b9b836a91df2921b425c952f3ac002b62dc2ff0
Akash <akashrajmane23@gmail.com>
2023-11-12 17:37:40 +0530

    conditional rendering

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -7,20 +7,25 @@ const Expenses = (props) => {
   const filteredArray = props.expenses
             .filter((el) => {
               const date = new Date(el.date).getFullYear();
+              if(props.selected==="All"){
+                return el;
+              }
               return date == props.selected;
             })
             .map((el) => (
                <ExpenseItem key={el.id} title={el.title} amount={el.amount} date={el.date} location={el.location} deleteHandler={() => props.deleteHandler(el.id)} />
             ));
 
+  const mappedArray =  props.expenses.map(el => (
+    <ExpenseItem key={el.id} title={el.title} amount={el.amount} date={el.date} location={el.location} deleteHandler={() => props.deleteHandler(el.id)} />
+  ))     
+  
+
   return (
     <Card>
-      {props.selected === ""
-        ? props.expenses.map(el => (
-            <ExpenseItem key={el.id} title={el.title} amount={el.amount} date={el.date} location={el.location} deleteHandler={() => props.deleteHandler(el.id)} />
-          ))
-        : filteredArray.length>0 ? filteredArray : <h1 style={{color:"#fff"}}>No Data Found</h1>
-      }
+      {props.expenses.length===0 && <h1 style={{color:"#fff"}}>No Data found</h1>}
+      {props.selected === "" ? mappedArray: filteredArray.length>0 ? filteredArray : (filteredArray.length===0 && props.expenses.length!==0) && <h1 style={{color:"#fff"}}>No Data found</h1>}
+      {(filteredArray.length!==0 && mappedArray.length===1) && <h1 style={{color:"#fff"}}>Only Single Expense here. Please add more.</h1>}
     </Card>
   );
 }
