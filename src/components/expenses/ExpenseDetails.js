2fe733247e4f596d4be964ad0c82613c0ca7932a
Akash <akashrajmane23@gmail.com>
2023-11-12 12:12:15 +0530

    added change amount fn

--- src/components/expenses/ExpenseDetails.js
+++ src/components/expenses/ExpenseDetails.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const ExpenseDetails = (props) => {
+  const [amount, setAmount] = useState(props.amount);
+  
   return (
     <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>${amount}</div>
+        <button onClick={()=>setAmount(100)}>change amount</button>
         <div className='expense-item__location'>{props.location}</div>
+
     </div>
   )
 }
