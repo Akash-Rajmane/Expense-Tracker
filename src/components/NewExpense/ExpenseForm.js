de12bc05b32104199c4c4af2eccb9c0274ad4ce8
Akash <akashrajmane23@gmail.com>
2023-11-12 13:50:58 +0530

    converted multiple states to single state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,35 +2,58 @@ import React,{useState} from 'react';
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-const [title,setTitle] = useState("");
-const [amount,setAmount] = useState("");
-const [date,setDate] = useState("");
+const [userInput, setUserInput] = useState({
+    title: "",
+    amount: "",
+    date: ""
+});
+// const [title,setTitle] = useState("");
+// const [amount,setAmount] = useState("");
+// const [date,setDate] = useState("");
 
 const titleChangeHandler = (e) => {
-    setTitle(e.target.value);
+    //setTitle(e.target.value)
+    setUserInput((prevState) => {
+         return {
+            ...prevState,
+            title: e.target.value
+        }
+    })
 }
 
 const amountChangeHandler = (e) => {
-    setAmount(e.target.value);
+    //setAmount(e.target.value);
+    setUserInput((prevState) => {
+        return {
+           ...prevState,
+           amount: e.target.value
+       }
+   })
 }
 
 const dateChangeHandler = (e) => {
-    setDate(e.target.value);
+    //setDate(e.target.value);
+    setUserInput((prevState) => {
+        return {
+           ...prevState,
+           date: e.target.value
+       }
+   })
 }
   return (
     <form>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler} value={title}/>
+                <input type="text" onChange={titleChangeHandler} value={userInput.title}/>
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={amount}/>
+                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={userInput.amount}/>
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type="date" min="2020-01-01" max="2024-12-31" onChange={dateChangeHandler} value={date}/>
+                <input type="date" min="2020-01-01" max="2024-12-31" onChange={dateChangeHandler} value={userInput.date}/>
             </div>
         </div>
         <div className='new-expense__actions'>
